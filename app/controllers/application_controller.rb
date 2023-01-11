63d07f5e3911e0590b7cd329cdbdbd0258c0f21c
+ <+>
2023-01-10 20:16:42 -0500

    user get route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
+  get '/users' do
+    users = User.all.order(:created_at)
+    users.to_json
+  end
 end
