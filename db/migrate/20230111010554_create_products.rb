a3fed5a23efff6a8195486bd27ca0a9c2eb6d07b
+ <+>
2023-01-10 20:07:58 -0500

    add columns to migrations

--- db/migrate/20230111010554_create_products.rb
+++ db/migrate/20230111010554_create_products.rb
@@ -1,4 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
+    create_table :products do |t|
+      t.string :productname
+      t.text :description
+      t.boolean :available
+      t.integer :cost
+      t.integer :price
+      t.string :category
+      t.string :img_url
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
