9ad1d2cb9c98b643438583d43c93763a346fd3cb
+ <+>
2023-01-10 20:12:19 -0500

    validate displayname presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_many :products
 
-    validates :username, presence: true
+    validates :displayname, :username, presence: true
     validates :username, uniqueness: true
 end
